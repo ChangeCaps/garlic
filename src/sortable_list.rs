a397c44a3f69aa8d237bd968aeb6857ce0b8fb17
Hjalte Nannestad <hjalte.nannestad@protonmail.com>
2022-12-07 01:38:55 +0100

    add user-select: none to list items

--- src/sortable_list.rs
+++ src/sortable_list.rs
@@ -232,6 +232,7 @@ fn render_items(
 
         let style = Style::new()
             .with("position", "absolute")
+            .with("user-select", "none")
             .with("left", format!("{}px", x))
             .with("top", format!("{}px", y));
 
